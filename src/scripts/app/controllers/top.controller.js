b2860f8341bc7bb55350c8b31645a84726c07702
Shinsuke Nishio <nishio@densan-labs.net>
2016-03-06 09:54:22 +0000

    データをremoteから読み込むように修正

--- src/scripts/app/controllers/top.controller.js
+++ src/scripts/app/controllers/top.controller.js
@@ -1,43 +1,13 @@
 'use strict';
 
 var mCtrls = require('./_mCtrls'),
-    mServices = require('../services/_mServices'),
     debug = require('debug'),
     log = debug('Ctrls'),
     loader = require('../../utilities/loader');
 
 mCtrls.controller('TopController', ['$scope', 'RecentProjectService', function ($scope, RecentProjectService) {
     // 新着プロジェクト
-    $scope.recentProjects = [
-        {
-            'name': 'rake',
-            'owner_name': 'testuser',
-            'github_url': 'http://github.com/rake/rake',
-            'description': ':package: A dynamic way of exploring and packaging REST APIs.',
-            'github_created_at': '2015-01-01 03:00:00',
-            'github_updated_at': '2015-01-01 03:00:00',
-            'stargazers_count': 30,
-            'watchers_count': 30
-        },
-        {
-            'name': 'rake',
-            'owner_name': 'testuser',
-            'github_url': 'http://github.com/rake/rake',
-            'description': ':package: A dynamic way of exploring and packaging REST APIs.',
-            'github_created_at': '2015-01-01 03:00:00',
-            'github_updated_at': '2015-01-01 03:00:00',
-            'stargazers_count': 30,
-            'watchers_count': 30
-        },
-        {
-            'name': 'rake',
-            'owner_name': 'testuser',
-            'github_url': 'http://github.com/rake/rake',
-            'description': ':package: A dynamic way of exploring and packaging REST APIs.',
-            'github_created_at': '2015-01-01 03:00:00',
-            'github_updated_at': '2015-01-01 03:00:00',
-            'stargazers_count': 30,
-            'watchers_count': 30
-        }
-    ];
+    RecentProjectService.query({}, function(response) {
+        $scope.recentProjects = response.results;
+    });
 }]);
--- src/scripts/app/services/recent-project.service.js
+++ src/scripts/app/services/recent-project.service.js
@@ -5,10 +5,10 @@ var mServices = require('./_mServices'),
     log = debug('Ctrls'),
     loader = require('../../utilities/loader');
 
-var baseUrl = 'http://192.168.0.19:3005/data';
+var baseUrl = 'http://192.168.0.19:3000/data';
 
 mServices.factory('RecentProjectService', function ($resource) {
-    // return $resource(baseUrl + "/recent_projects/index.json", {}, {
-    //     query { method: 'GET', params: {} }
-    // });
+    return $resource(baseUrl + "/recent_projects/index.json", {}, {
+        query: { method: 'GET', params: {} }
+    });
 });
