78e55c112000efa5e553900782b6eeaf79551e5f
Shinsuke Nishio <nishio@densan-labs.net>
2016-03-06 07:52:18 +0000

    dummy data作成

--- src/scripts/app/controllers/top.controller.js
+++ src/scripts/app/controllers/top.controller.js
@@ -6,7 +6,37 @@ var mCtrls = require('./_mCtrls'),
     loader = require('../../utilities/loader');
 
 mCtrls.controller('TopController', function ($scope) {
-    log('test');
-    $scope.test = 'test';
-    console.log(loader.getLoader('main').getResult('app-data'));
+    // 新着プロジェクト
+    $scope.recentProjects = [
+        {
+            'name': 'rake',
+            'owner_name': 'testuser',
+            'github_url': 'http://github.com/rake/rake',
+            'github_description': ':package: A dynamic way of exploring and packaging REST APIs.',
+            'github_created_at': '2015-01-01 03:00:00',
+            'github_updated_at': '2015-01-01 03:00:00',
+            'stargazers_count': 30,
+            'watchers_count': 30
+        },
+        {
+            'name': 'rake',
+            'owner_name': 'testuser',
+            'github_url': 'http://github.com/rake/rake',
+            'github_description': ':package: A dynamic way of exploring and packaging REST APIs.',
+            'github_created_at': '2015-01-01 03:00:00',
+            'github_updated_at': '2015-01-01 03:00:00',
+            'stargazers_count': 30,
+            'watchers_count': 30
+        },
+        {
+            'name': 'rake',
+            'owner_name': 'testuser',
+            'github_url': 'http://github.com/rake/rake',
+            'github_description': ':package: A dynamic way of exploring and packaging REST APIs.',
+            'github_created_at': '2015-01-01 03:00:00',
+            'github_updated_at': '2015-01-01 03:00:00',
+            'stargazers_count': 30,
+            'watchers_count': 30
+        }
+    ];
 });
