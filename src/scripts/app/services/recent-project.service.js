3d0a60c7cf24f0ddadcb1d2e41b44af47dc33b35
Shinsuke Nishio <nishio@densan-labs.net>
2016-03-06 12:16:42 +0000

    API_BASEは環境変数から読み込むようにした

--- src/scripts/app/services/recent-project.service.js
+++ src/scripts/app/services/recent-project.service.js
@@ -3,11 +3,14 @@
 var mServices = require('./_mServices'),
     debug = require('debug'),
     log = debug('Ctrls'),
+    environment = require('../../data/environment.json'),
     loader = require('../../utilities/loader');
 
-var baseUrl = 'http://192.168.0.19:3000/data';
+mServices
+.factory('RecentProjectService', function ($resource) {
 
-mServices.factory('RecentProjectService', function ($resource) {
+    var baseUrl = environment.API_BASE;
+
     return $resource(baseUrl + "/recent_projects.json", {}, {
         query: { method: 'GET', params: {} }
     });
