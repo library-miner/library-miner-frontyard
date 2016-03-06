ce5d4ca9422269c4c7c436e399062761ab41edce
Shinsuke Nishio <nishio@densan-labs.net>
2016-03-06 09:59:33 +0000

    インデント訂正

--- src/scripts/app/controllers/top.controller.js
+++ src/scripts/app/controllers/top.controller.js
@@ -5,7 +5,8 @@ var mCtrls = require('./_mCtrls'),
     log = debug('Ctrls'),
     loader = require('../../utilities/loader');
 
-mCtrls.controller('TopController', ['$scope', 'RecentProjectService', function ($scope, RecentProjectService) {
+mCtrls
+.controller('TopController', ['$scope', 'RecentProjectService', function ($scope, RecentProjectService) {
     // 新着プロジェクト
     RecentProjectService.query({ page: 1, per: 10 }, function(response) {
         $scope.recentProjects = response.results;
