4d1ab353ed72139420468cfa31a5bf4cf78e4aea
Shinsuke Nishio <nishio@densan-labs.net>
2016-03-20 10:46:10 +0000

    プロジェクト・ライブラリ検索でsortできるようにした

--- src/scripts/app/constants/constants.js
+++ src/scripts/app/constants/constants.js
@@ -7,5 +7,11 @@ mConstants.constant('Constants', {
     ProjectType: {
         project: 1,
         library: 2
-    }
+    },
+    projectSortOrder: [
+        ["stargazers_count desc", "スター数 Desc"],
+        ["stargazers_count asc", "スター数 Asc"],
+        ["github_updated_at desc", "最終更新日 Desc"],
+        ["github_updated_at asc", "最終更新日 Asc"]
+    ]
 });
