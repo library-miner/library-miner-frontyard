8e26c4c169921d2e649c20ec56c049923bb7b9df
Shinsuke Nishio <nishio@densan-labs.net>
2016-03-27 12:07:03 +0000

    deployスクリプト修正

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ touch environments/environment.json
 cp environments/{environment.json,environment.json.backup}
 cp environments/{environment.json.production,environment.json}
 ENV=production gulp build --dist
-cp environments/{environment.json.backup,environment.json}
 rsync -e ssh -avz ./website/ library-miner-web:/var/www/library-miner-frontyard
+cp environments/{environment.json.backup,environment.json}
